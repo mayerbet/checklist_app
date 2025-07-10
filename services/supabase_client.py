7002d75f54dab62ed79b817fac3c99b8421d3a14
mayerbet <meier.bet365@gmail.com>
2025-07-10 20:32:26 -0300

    Update supabase_client.py

--- services/supabase_client.py
+++ services/supabase_client.py
@@ -1,1 +1,7 @@
+from supabase import create_client
+import streamlit as st
+
+SUPABASE_URL = st.secrets["supabase"]["url"]
+SUPABASE_KEY = st.secrets["supabase"]["key"]
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
